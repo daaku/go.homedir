466a2f6be70db19a664f8a2276560781da27a74c
Naitik Shah <n@daaku.org>
2012-04-09 12:57:01 -0700

    initial commit

---
+++ homedir.go
@@ -0,0 +1,22 @@
+package homedir
+
+import (
+	"os/user"
+	"path"
+	"runtime"
+)
+
+// Default Home directory for the current user
+func Get() string {
+	user, err := user.Current()
+	if err != nil {
+		return "/"
+	}
+	username := user.Username
+
+	switch runtime.GOOS {
+	case "darwin":
+		return path.Join("/Users", username)
+	}
+	return path.Join("/home", username)
+}
